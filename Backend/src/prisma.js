50a41d3d4045586f4cfdd9d0de86f7c1ef12a429
Will Macaulay <58277689+will-macaulay@users.noreply.github.com>
2026-01-17 03:30:05 -0800

    another fix to render security error

--- Backend/src/prisma.js
+++ Backend/src/prisma.js
@@ -3,11 +3,9 @@ import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: isProd ? { rejectUnauthorized: false } : false,
+    ssl: { rejectUnauthorized: false }
 });
 
 const adapter = new PrismaPg(pool);
