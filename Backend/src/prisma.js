80e652d65ab4cc823b0262e1c234ec65d993bb71
Will Macaulay <58277689+will-macaulay@users.noreply.github.com>
2026-01-17 22:59:22 -0600

    render security error fix -- no longer rejecting unauthorized ssl

--- Backend/src/prisma.js
+++ Backend/src/prisma.js
@@ -1,18 +1,10 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const ca = process.env.SUPABASE_CA_CERT
-  ? process.env.SUPABASE_CA_CERT.replace(/\\n/g, "\n")
-  : undefined;
-
-const pool = new Pool({
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
-  ssl: ca
-    ? { ca, rejectUnauthorized: true }
-    : { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
 
-const adapter = new PrismaPg(pool);
 export const prisma = new PrismaClient({ adapter });
